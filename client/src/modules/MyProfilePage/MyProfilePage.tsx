c90d5c8a030594b2f5c31c5800415827154e2dc2
Ivan Zivkovic <imzivko@gmail.com>
2024-04-17 10:02:18 +0200

    remove deduplication data wipe button for users

--- client/src/modules/MyProfilePage/MyProfilePage.tsx
+++ client/src/modules/MyProfilePage/MyProfilePage.tsx
@@ -2,8 +2,13 @@ import { PageContainer } from '@/components/PageContainer';
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card';
 import { useUserMe } from '@/services/users/api';
 import { MyProfileForm, WipeDataDialog } from '@/modules/MyProfilePage/components';
+import { useAuth } from '@/providers/GlobalProvider';
+import { UserRole } from '@/services/users';
 
 export const MyProfilePage = () => {
+  const {
+    user: { role },
+  } = useAuth();
   const { data: userProfileData, isLoading: userProfileQueryLoading } = useUserMe({ queryEnabled: true });
 
   return (
@@ -12,7 +17,7 @@ export const MyProfilePage = () => {
       isLoading={userProfileQueryLoading}
       containerClassName="sm:p-6 p-0 pt-6"
       headerClassName="sm:px-0 px-6"
-      headerNode={<WipeDataDialog />}
+      headerNode={role === UserRole.Admin && <WipeDataDialog />}
     >
       <div className="space-y-8 pb-6 max-w-3xl">
         <Card className="sm:bg-secondary/20 border-0 sm:border sm:dark:bg-secondary/10 shadow-none">
