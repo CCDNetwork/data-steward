ca80b8667751ef916aae676c6b32e79eee28180c
Luka Radenovic <luka.radenovic2@gmail.com>
2024-05-17 12:18:00 +0200

    Remove unused 'Order' property from BeneficiaryAttributeGroupCreateRequest

--- server/Ccd.Server/Areas/BeneficiaryAttributes/Controllers/ControllerModels/BeneficiaryAttributeGroupCreateRequest.cs
+++ server/Ccd.Server/Areas/BeneficiaryAttributes/Controllers/ControllerModels/BeneficiaryAttributeGroupCreateRequest.cs
@@ -7,7 +7,6 @@ namespace Ccd.Server.BeneficiaryAttributes;
 public class BeneficiaryAttributeGroupCreateRequest
 {
     [Required] public string Name { get; set; }
-    [Required, Range(1, int.MaxValue)] public int Order { get; set; }
     [Required] public bool IsActive { get; set; }
     [Required] public bool UseFuzzyMatch { get; set; }
     [Required][MinLength(1)] public List<int> BeneficiaryAttributeIds { get; set; }
