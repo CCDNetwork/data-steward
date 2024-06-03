9e0339a2132c7ddfe26d17623111ee47d01ea425
Luka Radenovic <luka.radenovic2@gmail.com>
2024-06-03 12:30:16 +0200

    Add support for handling user permissions in UserPatchRequest

--- server/Ccd.Server/Areas/Users/Controllers/ControllerModels/UserPatchRequest.cs
+++ server/Ccd.Server/Areas/Users/Controllers/ControllerModels/UserPatchRequest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Ccd.Server.Helpers;
 
 namespace Ccd.Server.Areas.Users.Controllers.ControllerModels;
@@ -7,4 +8,5 @@ public class UserPatchRequest : PatchRequest
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Password { get; set; }
+    public List<string> Permissions { get; set; }
 }
