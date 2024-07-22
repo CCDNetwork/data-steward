0bf668d9948981e89fbe86f5de037bee886c7564
Luka Radenovic <luka.radenovic2@gmail.com>
2024-07-22 11:36:48 +0200

    Add security headers to Vite server configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -19,5 +19,12 @@ export default defineConfig({
   server: {
     open: true,
     port: 3000,
+    headers: {
+      'Strict-Transport-Security': 'max-age=86400; includeSubDomains', // Adds HSTS options to your website, with a expiry time of 1 day
+      'X-Content-Type-Options': 'nosniff', // Protects from improper scripts runnings
+      'X-Frame-Options': 'DENY', // Stops your site being used as an iframe
+      'X-XSS-Protection': '1; mode=block', // Gives XSS protection to legacy browsers
+      'Content-Security-Policy': 'upgrade-insecure-requests',
+    },
   },
 });
